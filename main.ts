eed034131c3c61a0f5bfb6fbb21e9e723613a054
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-09-21 09:30:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    number = Math.trunc(23.977)
+    basic.showNumber(number)
+})
+let number = 0
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
